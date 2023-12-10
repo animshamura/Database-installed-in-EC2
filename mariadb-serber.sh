cd2f07d62e4fb10e4998ddcb93db3e317f42d500
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2023-12-11 01:13:09 +0600

    Create mariadb-serber.sh

---
+++ mariadb-serber.sh
@@ -0,0 +1,18 @@
+sudo apt update 
+
+sudo apt install mariadb-server 
+
+sudo mysql_secure_installation
+
+#bind address to 0.0.0.0 thus any remote login is accessible in the database
+
+mysql vi etc/mysql/mariadb.conf.d/50-server.cnf
+
+sudo systemctl restart mysql 
+
+mysql -h 3.71.41.101 -u root -p 
+
+
+
+
+
